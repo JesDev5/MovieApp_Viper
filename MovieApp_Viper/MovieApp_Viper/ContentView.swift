22a4302b34fd68aca5917b6cfdae14dd5fb57bb2
Jess <jesdev25@gmail.com>
2024-10-21 09:07:41 -0500

    init Project

---
+++ MovieApp_Viper/MovieApp_Viper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieApp_Viper
+//
+//  Created by JessMac on 21/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieApp_Viper/MovieApp_Viper/MovieApp_ViperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp_ViperApp.swift
+//  MovieApp_Viper
+//
+//  Created by JessMac on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp_ViperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
