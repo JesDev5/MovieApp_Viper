a535b1e785aa279037d1e6642a9c3b774ed3d2fb
Jess <jesdev25@gmail.com>
2024-10-21 17:00:30 -0500

    project folders were updated

---
+++ MovieApp_Viper/MovieApp_Viper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp_Viper
+//
+//  Created by JessMac on 21/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
