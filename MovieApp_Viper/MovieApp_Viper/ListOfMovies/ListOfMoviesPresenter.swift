b808aeafaa9a9f865c28a08e2ca41555f2c73c54
Jess <jesdev25@gmail.com>
2024-10-22 11:08:26 -0500

    Add Architecture files

---
+++ MovieApp_Viper/MovieApp_Viper/ListOfMovies/ListOfMoviesPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  ListOfMoviesPresenter.swift
+//  MovieApp_Viper
+//
+//  Created by JessMac on 21/10/24.
+//
+
+import Foundation
---
+++ MovieApp_Viper/MovieApp_Viper/ListOfMovies/ListOfMoviesRouter.swift
@@ -0,0 +1,8 @@
+//
+//  listOfMoviesRouter.swift
+//  MovieApp_Viper
+//
+//  Created by JessMac on 21/10/24.
+//
+
+import Foundation
