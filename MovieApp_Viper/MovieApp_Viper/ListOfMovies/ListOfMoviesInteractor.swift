43832fc9d6a5485a20d43254911287599cf39e8b
Jess <jesdev25@gmail.com>
2024-11-07 12:43:02 -0500

    delete extra space

--- MovieApp_Viper/MovieApp_Viper/ListOfMovies/ListOfMoviesInteractor.swift
+++ MovieApp_Viper/MovieApp_Viper/ListOfMovies/ListOfMoviesInteractor.swift
@@ -23,7 +23,6 @@ class ListOfMoviesInteractor: ListOfMoviesBusinessLogic {
     func getListOfMovies() async throws -> PopularMovieResponseEntity? {
         let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key= 6db0403d1c20559e1d5e62d5eecdf34c")!
         let (data, _) = try! await URLSession.shared.data(from: url)
-        
         return try! JSONDecoder().decode(PopularMovieResponseEntity.self, from: data)
         
    }
