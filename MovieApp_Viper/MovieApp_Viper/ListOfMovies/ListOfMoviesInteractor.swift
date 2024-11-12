85002bf8797c31f5d1a50726484bc95f3d196d80
Jess <jesdev25@gmail.com>
2024-11-12 09:48:15 -0500

    call the getList function from the presenter

--- MovieApp_Viper/MovieApp_Viper/ListOfMovies/ListOfMoviesInteractor.swift
+++ MovieApp_Viper/MovieApp_Viper/ListOfMovies/ListOfMoviesInteractor.swift
@@ -9,7 +9,7 @@ import Foundation
 //el protocolo define reglas que una clase debe cumplir
 protocol ListOfMoviesBusinessLogic  {
                           //asincrona y devuelve un objeto de tipo o nil o caso de error
-    func getListOfMovies() async throws -> PopularMovieResponseEntity?
+    func getListOfMovies() async -> PopularMovieResponseEntity
 }
 
 //define esta clase que implemente el protocolo
@@ -20,7 +20,7 @@ class ListOfMoviesInteractor: ListOfMoviesBusinessLogic {
         self.presenter = presenter
     }
     
-    func getListOfMovies() async throws -> PopularMovieResponseEntity? {
+    func getListOfMovies() async -> PopularMovieResponseEntity {
         let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key= 6db0403d1c20559e1d5e62d5eecdf34c")!
         let (data, _) = try! await URLSession.shared.data(from: url)
         return try! JSONDecoder().decode(PopularMovieResponseEntity.self, from: data)
