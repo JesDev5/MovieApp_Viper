42fbff3977fdbea756baeeb213eb1792be5a10e0
Jess <jesdev25@gmail.com>
2024-11-21 10:45:28 -0500

    the API address is fixed

--- MovieApp_Viper/MovieApp_Viper/ListOfMovies/ListOfMoviesInteractor.swift
+++ MovieApp_Viper/MovieApp_Viper/ListOfMovies/ListOfMoviesInteractor.swift
@@ -15,7 +15,8 @@ class ListOfMoviesInteractor: ListOfMoviesBusinessLogic {
     var presenter: ListOfMoviesPresentationLogic?
         
     func getListOfMovies() async -> PopularMovieResponseEntity {
-        let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key= 6db0403d1c20559e1d5e62d5eecdf34c")!
+        //let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key= 6db0403d1c20559e1d5e62d5eecdf34c")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=6db0403d1c20559e1d5e62d5eecdf34c")!
         let (data, _) = try! await URLSession.shared.data(from: url)
         let decodeData = try! JSONDecoder().decode(PopularMovieResponseEntity.self, from: data)
         return decodeData
