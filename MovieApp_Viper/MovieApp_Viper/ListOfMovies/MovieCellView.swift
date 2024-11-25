88bc234064e570a9a9c4c9c515860fec0a158a5b
Jess <jesdev25@gmail.com>
2024-11-25 12:06:42 -0500

    You must create the cell that when reused

---
+++ MovieApp_Viper/MovieApp_Viper/ListOfMovies/MovieCellView.swift
@@ -0,0 +1,56 @@
+//
+//  MovieCellView.swift
+//  MovieApp_Viper
+//
+//  Created by JessMac on 21/11/24.
+//
+
+import Foundation
+import UIKit
+
+class MovieCellView: UITableViewCell {
+    
+    let movieImageView: UIImageView = {
+        let imageView  = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    let movieName: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 2
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        return label
+    }()
+    
+    let movieDescription: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 2
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.textColor = .gray
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        
+    }
+  }
+
+
+    
+   
+    
+
+    
+   
+    
+    
+
