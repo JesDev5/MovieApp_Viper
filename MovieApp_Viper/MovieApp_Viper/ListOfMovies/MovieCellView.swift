736a63d73ffb315ceaa6d9ee1e16ac8be116b161
Jess <jesdev25@gmail.com>
2024-12-02 12:23:19 -0500

    set cells and define contrains

--- MovieApp_Viper/MovieApp_Viper/ListOfMovies/MovieCellView.swift
+++ MovieApp_Viper/MovieApp_Viper/ListOfMovies/MovieCellView.swift
@@ -36,11 +36,34 @@ class MovieCellView: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
         
+    }
+    
+    private func setupView() {
+        contentView.addSubview(movieImageView)
+        contentView.addSubview(movieName)
+        contentView.addSubview(movieDescription)
+        
+        NSLayoutConstraint.activate([
+            movieImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
+            movieImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            movieImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            movieImageView.widthAnchor.constraint(equalToConstant: 100),
+            movieImageView.heightAnchor.constraint(equalToConstant: 200),
+            
+            movieName.topAnchor.constraint(equalTo: movieImageView.topAnchor, constant: 24),
+            movieName.leadingAnchor.constraint(equalTo: movieImageView.trailingAnchor, constant: 18),
+            movieName.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
+            
+            movieDescription.topAnchor.constraint(equalTo: movieName.bottomAnchor, constant: 8),
+            movieDescription.leadingAnchor.constraint(equalTo: movieDescription.trailingAnchor, constant: 20),
+            movieDescription.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
+        ])
     }
   }
 
